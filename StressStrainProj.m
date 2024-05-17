34ba057e8a7c06a08922f60c6c64b1b41b1c762d
sbuare <166256487+sbuare@users.noreply.github.com>
2024-05-17 13:49:37 -0700

    Update StressStrainProj.m
    Fixed spelling errors, cleaned up code

--- StressStrainProj.m
+++ StressStrainProj.m
@@ -7,8 +7,8 @@ HIPS_file = 'HIPS_51_16_01_21_19_15.xlsx';
 nylon_file = 'Nylon6_61_16_01_21_19_20.xlsx';
 
 % User inputs
-cross_section = input('Is the cross_sectional area circular? [Y/N]\n', 's');
-area = input('What is the cross-setional area of the material? (Hint: probably 2.482 square mm)\n');
+cross_section = input('Is the cross-sectional area circular? [Y/N]\n', 's');
+area = input('What is the cross-sectional area of the material? (Hint: probably 2.482 square mm)\n');
 diameter = input('What is the diameter? (Hint: probably 1.7777 mm)\n');
 g_len = input('What is the gauge length? (Hint: probably 18 mm)\n');
 
@@ -20,24 +20,25 @@ material = input(mat_string);
 % Values in MPa of elastic modulus, ultimate stress, fracture stress
 % Plots of elongation and stress strain
 
+output_str = ' Modulus of elasticity: %.2f MPa\n Ultimate stress: %.2f MPa\n Fracture stress: %.2f MPa\n';
 while material ~= -1
     switch material
         case 1
             [poly_elast, poly_ult_stress, poly_fract_stress] = analyze(poly_file, area, g_len);
             disp('Polypropylene:')
-            fprintf(' Modulus of elasticity: %.2f MPa\n Ultimate stress: %.2f MPa\n Fracture stress: %.2f MPa\n', poly_elast, poly_ult_stress, poly_fract_stress);
+            fprintf(output_str, poly_elast, poly_ult_stress, poly_fract_stress);
         case 2
             [ABS_elast, ABS_ult_stress, ABS_fract_stress] = analyze(ABS_file, area, g_len);
             disp('ABS:')
-            fprintf(' Modulus of elasticity: %.2f MPa\n Ultimate stress: %.2f MPa\n Fracture stress: %.2f MPa\n', ABS_elast, ABS_ult_stress, ABS_fract_stress);
+            fprintf(output_str, ABS_elast, ABS_ult_stress, ABS_fract_stress);
         case 3
             [HIPS_elast, HIPS_ult_stress, HIPS_fract_stress] = analyze(HIPS_file, area, g_len);
             disp('HIPS:')
-            fprintf(' Modulus of elasticity: %.2f MPa\n Ultimate stress: %.2f MPa\n Fracture stress: %.2f MPa\n', HIPS_elast, HIPS_ult_stress, HIPS_fract_stress);
+            fprintf(output_str, HIPS_elast, HIPS_ult_stress, HIPS_fract_stress);
         case 4
             [nylon_elast, nylon_ult_stress, nylon_fract_stress] = analyze(nylon_file, area, g_len);
             disp('Nylon:')
-            fprintf(' Modulus of elasticity: %.2f MPa\n Ultimate stress: %.2f MPa\n Fracture stress: %.2f MPa\n', nylon_elast, nylon_ult_stress, nylon_fract_stress);
+            fprintf(output_str, nylon_elast, nylon_ult_stress, nylon_fract_stress);
         otherwise
             disp('Invalid input. Please try again.')
     end
@@ -45,5 +46,4 @@ while material ~= -1
 end
 
 disp('Thank you for using the stress-strain analyzer.')
-
 
